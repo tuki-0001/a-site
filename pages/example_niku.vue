51ff3b109c5b65638d5d20a442582edade733c46
tuki-0001 <ktc24a31f0001@edu.kyoto-tech.ac.jp>
2024-10-21 14:26:22 +0900

    niku

---
+++ pages/example_niku.vue
@@ -0,0 +1,112 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container mt-3">
+    <h1 class="display-1">美味しさNo.１！KFC風フライドチキン</h1>
+
+    <p class="h2 my-5">KFC人気レシピ1位！数あるKFCレシピ─どれも何か一味足りない気が。試行錯誤の末の主人も絶賛☆再現率の高いレシピです☆</p>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-4 border p-2">
+        <h2>材料</h2>
+        <ul class="list-unstyled">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              <li>鶏肉(お好きな部位)　２〜３枚位</li>
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              <li>☆牛乳　1/2カップ</li>
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              <li>☆卵　１個</li>
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              <li>☆にんにくチューブ　２センチ位</li>
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              <li>★薄力粉　１カップ</li>
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              <li>★オールスパイス　小さじ１弱</li>
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              <li>★顆粒コンソメ　大さじ1/2</li>
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              <li>★塩　大さじ1/2</li>
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              <li>あれば★パプリカパウダー　小さじ１</li>
+            </label>
+          </div>
+        </ul>
+      </div>
+      <div class="col-12 col-md-4 border px-5">
+        <h2>作り方</h2>
+        <ol>
+          <li>☆をボウルに混ぜ合わせておく</li>
+          <li>ボウルに★を全て混ぜ合わせておく。</li>
+          <li>肉の厚みがある部分を切り開き、厚みを一定にする。鶏肉１枚を２～３等分の大きめに切る。</li>
+          <li>１のボウルに鶏肉を浸す。<mark>(時間があれば20分以上浸す。)</mark></li>
+          <li>２のボウルに鶏肉を入れて粉類をまんべんなくまぶし、別の容器に並べていく。粉が余るようでしたら、二度付けをお勧めします！</li>
+          <li>卵液→粉→卵液→粉と二度付けする事で、カリっと感がアップ＋味もしっかり付きます！写真の我が家のは二度付けしています。</li>
+          <li>油を160度に温める。(温めるのはこのタイミングで大丈夫です。待ってる間に衣が鶏肉になじみます。)</li>
+          <li>160度で５分以上、様子を見ながら、火が通ったら、一度上げて、今度は180～200度でカリっと仕上げ、完成です☆</li>
+          <li>鶏肉の1枚はサイズにばらつきがある為、2～3枚という記載に変更しました。</li>
+        </ol>
+      </div>
+      <div class="col-12 col-md-4 border py-4">
+        <div class="text-bg-danger p-3">
+          <h2>コツ・ポイント</h2>
+        </div>
+        <p class="lead">揚げる際鍋底での焦げ防止に衣が軽く揚がるまで菜箸等で時々浮かせて下さい</p>
+        <p class="lead">好みで粉類に粗びき黒胡椒小さじ1/2を足しても◎</p>
+        <p class="lead">にんにくチューブ２センチはガーリックパウダー小さじ１に変えて粉類と混ぜても美味しいです。</p>
+        <p class="lead">パプリカパウダーは無くても可</p>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <figure class="text-center">
+      <blockquote class="blockquote">
+        <h2>このレシピの生い立ち</h2>
+      </blockquote>
+      <figcaption class="blockquote-footer">
+        <p>今まで作ったKFCレシピは、<small>卵液</small>や粉もかなり余るし、何よりも</p>
+        <p>何かひと味足りない……と思って、やっとKFC味にたどり着きました！</p>
+        <p>どの家庭にもあるような<small>材料</small>で☆</p>
+        <p>牛乳と卵液、粉類も必要量のみになっているので無駄が無く<small>経済的</small>です☆></p>
+      </figcaption>
+    </figure>
+  </div>
+</template>
+
+<style></style>
