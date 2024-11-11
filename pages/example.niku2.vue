5b68818dbda709c198832ab69d3cdbbd13ec8e20
tuki-0001 <ktc24a31f0001@edu.kyoto-tech.ac.jp>
2024-11-11 14:27:09 +0900

    ショート

--- pages/example.niku2.vue
+++ pages/example.niku2.vue
@@ -4,67 +4,65 @@
 
 <template>
 <div class="container mt-3">
-  <h1 class="display-1">美味しさNo.１！KFC風フライドチキン</h1>
-
-  <p class="h2 my-5">KFC人気レシピ1位！数あるKFCレシピ─どれも何か一味足りない気が。試行錯誤の末の主人も絶賛☆再現率の高いレシピです☆</p>
+  <h1 class="display-1">僕のヒーローアカデミアキャラ紹介</h1>
 </div>
 <div class="container">
   <div class="row">
     <div class="col-12 col-md-4 border p-2">
-      <h2>材料</h2>
+      <h2>轟焦凍</h2>
       <ul class="list-unstyled">
-        <div class="form-check">
+       <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
           <label class="form-check-label" for="flexCheckDefault">
-            <li>鶏肉(お好きな部位)　２〜３枚位</li>
+            <li>ヒーロー名　ショート</li>
           </label>
         </div>
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
           <label class="form-check-label" for="flexCheckDefault">
-            <li>☆牛乳　1/2カップ</li>
+            <li>個性　半冷半熱</li>
           </label>
         </div>
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
           <label class="form-check-label" for="flexCheckDefault">
-            <li>☆卵　１個</li>
+            <li>学校学科　雄英高校ヒーロー科1年A組15番</li>
           </label>
         </div>
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
           <label class="form-check-label" for="flexCheckDefault">
-            <li>☆にんにくチューブ　２センチ位</li>
+            <li>出身校　凝山中学校</li>
           </label>
         </div>
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
           <label class="form-check-label" for="flexCheckDefault">
-            <li>★薄力粉　１カップ</li>
+            <li>誕生日　1月11日</li>
           </label>
         </div>
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
           <label class="form-check-label" for="flexCheckDefault">
-            <li>★オールスパイス　小さじ１弱</li>
+            <li>身長176cm</li>
           </label>
         </div>
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
           <label class="form-check-label" for="flexCheckDefault">
-            <li>★顆粒コンソメ　大さじ1/2</li>
+            <li>血液型　O型</li>
           </label>
         </div>
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
           <label class="form-check-label" for="flexCheckDefault">
-            <li>★塩　大さじ1/2</li>
+            <li>好きなもの　蕎麦（温かくないやつ）</li>
           </label>
         </div>
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
           <label class="form-check-label" for="flexCheckDefault">
-            <li>あれば★パプリカパウダー　小さじ１</li>
+            <li>性格　クール＆ホットな天然</li>
           </label>
         </div>
       </ul>
