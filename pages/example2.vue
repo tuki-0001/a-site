9ec6c4cc6c95a51570610749d40bb668db7b18aa
tuki-0001 <ktc24a31f0001@edu.kyoto-tech.ac.jp>
2025-01-25 14:50:00 +0900

    ヴィラン追加

--- pages/example2.vue
+++ pages/example2.vue
@@ -1,264 +1,30 @@
 <script setup>
 const characters = [
   {
+    rank: 8,
+    name: "荼毘",
+    aliasType: "ヴィラン名", // ヴィランの場合
+    alias: "荼毘",
+    ability: "蒼炎",
+    birthday: "1月18日",
+    height: "176cm",
+    bloodType: "不明",
+    likes: "蕎麦",
+    personality: "つかめない",
+    image: "https://via.placeholder.com/120", // 仮画像
+  },
+  {
+    rank: 1,
     name: "爆豪勝己",
-    heroName: "大・爆・殺・神ダイナマイト",
+    aliasType: "ヒーロー名", // ヒーローの場合
+    alias: "大・爆・殺・神ダイナマイト",
     ability: "爆破",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
     birthday: "4月20日",
     height: "172cm",
     bloodType: "A型",
     likes: "辛い食べ物全般、登山",
     personality: "暴言自信家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "轟焦凍",
-    heroName: "ショート",
-    ability: "半冷半熱",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "凝山中学校",
-    birthday: "1月11日",
-    height: "176cm",
-    bloodType: "O型",
-    likes: "蕎麦（温かくないやつ）",
-    personality: "クール＆ホットな天然",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "相澤消太",
-    heroName: "抹消ヒーローイレイザーヘッド",
-    ability: "抹消",
-    school: "ー",
-    formerSchool: "ー",
-    birthday: "11月8日",
-    height: "183cm",
-    bloodType: "B型",
-    likes: "ねこ",
-    personality: "なんだかんだあまい",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
-  },
-  {
-    name: "緑谷出久",
-    heroName: "デク",
-    ability: "ワン・フォー・オール",
-    school: "雄英高校ヒーロー科1年A組",
-    formerSchool: "折寺中学校",
-    birthday: "7月15日",
-    height: "166cm",
-    bloodType: "O型",
-    likes: "カツ丼、ヒーロー分析ノート",
-    personality: "真面目で努力家",
-    image: "https://via.placeholder.com/120", // キャラ画像
+    image: "https://via.placeholder.com/120", // 仮画像
   },
 ];
 </script>
@@ -267,11 +33,11 @@ const characters = [
   <div class="container mt-5">
     <div class="row">
       <!-- 各キャラクターカードをループで表示 -->
-      <div v-for="(character, index) in characters" :key="index" class="col-12 col-md-6 col-lg-4 mb-4">
+      <div v-for="character in characters" :key="character.rank" class="col-12 col-md-6 col-lg-4 mb-4">
         <div class="license-card mx-auto p-3 shadow">
           <!-- ヘッダー部分 -->
           <div class="license-header text-center mb-3">
-            <h2 class="h5 mb-0">{{ index + 1 }}位 {{ character.name }}</h2>
+            <h2 class="h5 mb-0">{{ character.rank }}位 {{ character.name }}</h2>
           </div>
           <!-- メイン部分 -->
           <div class="d-flex align-items-start">
@@ -283,21 +49,14 @@ const characters = [
             <div class="ms-3">
               <table class="table table-sm mb-0">
                 <tbody>
+                  <!-- ヒーロー名またはヴィラン名を表示 -->
                   <tr>
-                    <th>ヒーロー名</th>
-                    <td>{{ character.heroName }}</td>
+                    <th>{{ character.aliasType }}</th>
+                    <td>{{ character.alias }}</td>
                   </tr>
                   <tr>
                     <th>個性</th>
                     <td>{{ character.ability }}</td>
-                  </tr>
-                  <tr>
-                    <th>学科</th>
-                    <td>{{ character.school }}</td>
-                  </tr>
-                  <tr>
-                    <th>出身校</th>
-                    <td>{{ character.formerSchool }}</td>
                   </tr>
                   <tr>
                     <th>誕生日</th>
@@ -307,7 +66,7 @@ const characters = [
                     <th>身長</th>
                     <td>{{ character.height }}</td>
                   </tr>
-                  <tr>
+                  <tr v-if="character.bloodType">
                     <th>血液型</th>
                     <td>{{ character.bloodType }}</td>
                   </tr>
