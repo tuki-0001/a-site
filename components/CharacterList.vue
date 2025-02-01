b3c841db6aad5a9250e3fa6e531f5f1d2444ca0b
tuki-0001 <ktc24a31f0001@edu.kyoto-tech.ac.jp>
2025-02-01 17:26:37 +0900

    変更

--- components/CharacterList.vue
+++ components/CharacterList.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="character-list">
-    <div v-for="character in sortedCharacters" :key="character.rank" class="character-item">
+    <div v-for="character in sortedCharacters" :key="character.rank" class="character-item mb-3">
       <div class="d-flex p-3">
         <div class="character-image-wrapper">
-          <img :src="character.image" :alt="character.name" class="character-image">
+          <img :src="character.image" :alt="character.name" class="character-image rounded-circle">
         </div>
         <div class="ms-3">
           <div class="d-flex align-items-center">
-            <span class="rank-circle">{{ character.rank }}</span>
+            <span class="badge bg-warning text-dark me-2">{{ character.rank }}</span>
             <h3>{{ character.name }}</h3>
           </div>
           <p>Hero Name: {{ character.heroName }}</p>
@@ -26,14 +26,9 @@ props: {
 </script>
 
 <style scoped>
-.character-item {
-  border-bottom: 1px solid #ddd;
-}
-
 .character-image-wrapper {
   width: 120px;
   height: 120px;
-  border-radius: 8px;
   overflow: hidden;
 }
 
@@ -41,18 +36,5 @@ props: {
   width: 100%;
   height: 100%;
   object-fit: cover;
-}
-
-.rank-circle {
-  display: inline-block;
-  width: 30px;
-  height: 30px;
-  background-color: gold;
-  color: black;
-  font-weight: bold;
-  border-radius: 50%;
-  text-align: center;
-  line-height: 30px;
-  margin-right: 10px;
 }
 </style>
