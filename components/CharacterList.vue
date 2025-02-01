e15db3d9914b161b21455c793328ab5ac39bafc2
tuki-0001 <ktc24a31f0001@edu.kyoto-tech.ac.jp>
2025-02-01 17:19:32 +0900

    変更

--- components/CharacterList.vue
+++ components/CharacterList.vue
@@ -1,20 +1,58 @@
 <template>
-  <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-4 col-sm-6 mb-4" v-for="character in sortedCharacters" :key="character.rank">
-        <CharacterCard :character="character" />
+  <div class="character-list">
+    <div v-for="character in sortedCharacters" :key="character.rank" class="character-item">
+      <div class="d-flex p-3">
+        <div class="character-image-wrapper">
+          <img :src="character.image" :alt="character.name" class="character-image">
+        </div>
+        <div class="ms-3">
+          <div class="d-flex align-items-center">
+            <span class="rank-circle">{{ character.rank }}</span>
+            <h3>{{ character.name }}</h3>
+          </div>
+          <p>Hero Name: {{ character.heroName }}</p>
+          <p>Ability: {{ character.ability }}</p>
+          <p>Birthday: {{ character.birthday }}</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import CharacterCard from '@/components/CharacterCard.vue';
-
-defineProps({
-  sortedCharacters: {
-    type: Array,
-    required: true
-  },
-});
+props: {
+  sortedCharacters: Array
+}
 </script>
+
+<style scoped>
+.character-item {
+  border-bottom: 1px solid #ddd;
+}
+
+.character-image-wrapper {
+  width: 120px;
+  height: 120px;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+.character-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.rank-circle {
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  background-color: gold;
+  color: black;
+  font-weight: bold;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 30px;
+  margin-right: 10px;
+}
+</style>
