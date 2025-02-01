da8321724e8c8fb4bd33d035fd03669b1f7c951f
tuki-0001 <ktc24a31f0001@edu.kyoto-tech.ac.jp>
2025-02-01 18:30:48 +0900

    編集

--- components/CharacterCard.vue
+++ components/CharacterCard.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="d-flex align-items-start">
       <div class="character-image-wrapper">
-        <img :src="character.image || 'default-image.jpg'" alt="キャラクター画像" class="character-image" />
+        <img :src="character.image || require('@/assets/default-image.jpg')" :alt="`${character.name}の画像`" class="character-image" />
       </div>
       <div class="ms-3">
         <table class="table table-sm mb-0">
@@ -20,23 +20,23 @@
               <th>ヴィラン名</th>
               <td>{{ character.villainName }}</td>
             </tr>
-            <tr>
+            <tr v-if="character.ability">
               <th>個性</th>
               <td>{{ character.ability }}</td>
             </tr>
-            <tr>
+            <tr v-if="character.birthday">
               <th>誕生日</th>
               <td>{{ character.birthday }}</td>
             </tr>
-            <tr>
+            <tr v-if="character.height">
               <th>身長</th>
               <td>{{ character.height }}</td>
             </tr>
-            <tr>
+            <tr v-if="character.likes">
               <th>好きなもの</th>
               <td>{{ character.likes }}</td>
             </tr>
-            <tr>
+            <tr v-if="character.personality">
               <th>性格</th>
               <td>{{ character.personality }}</td>
             </tr>
